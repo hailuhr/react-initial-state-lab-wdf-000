1c1b2115ed0b948cd9e3b24a2a49fe9b8a961d0a
Flatiron School <github email address>
2016-12-26 17:16:31 -0500

    done

---
+++ components/Bomb.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+      <p>{this.state.secondsLeft ? `${this.state.secondsLeft} seconds left before I go boom!` : `Boom!`}</p>
+      </div>
+    );
+  }
+}
+
+
+module.exports = Bomb
---
+++ components/ImageSlider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class ImageSlider extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I am on slide {this.state.currentSlideIndex}</p>
+      </div>
+    )
+  }
+}
+
+
+module.exports = ImageSlider;
